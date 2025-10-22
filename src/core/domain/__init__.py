6b03f4805b36c16661de3e24f1ada6d2d6318e12
uladzislaufarshakou <uladzislau.farshakou@innowise.com>
2025-10-22 11:13:12 +0200

    Update __init__.py
    Setup __init__.py for future imports

---
+++ src/core/domain/__init__.py
@@ -0,0 +1,25 @@
+from .models import (
+    Role,
+    LearningState,
+    ReviewState,
+    User,
+    LearnedTechnology,
+    TraineeTechnologyState,
+    LearningSessionLog,
+    TechnologyReview,
+    StatusUpdate,
+    StatusFeedback,
+)
+
+__all__ = [
+    "Role",
+    "LearningState",
+    "ReviewState",
+    "User",
+    "LearnedTechnology",
+    "TraineeTechnologyState",
+    "LearningSessionLog",
+    "TechnologyReview",
+    "StatusUpdate",
+    "StatusFeedback",
+]
